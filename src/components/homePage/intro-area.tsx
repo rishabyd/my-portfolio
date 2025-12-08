88870683610e3c1a37570b6c071df243cc192ad0
rishabyd <elitebot47@gmail.com>
2025-12-08 19:41:13 +0530

    Update intro-area.tsx

--- src/components/homePage/intro-area.tsx
+++ src/components/homePage/intro-area.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import ProfilePictureArea from "./profile-picture-side/profile-picture-area";
-
 export default function IntroArea({ className }: { className?: string }) {
   return (
     <div className={`${className} font-mono lg:flex gap-5`}>
-      <div>
-        <ProfilePictureArea />
-      </div>
-
       <div className="">
-        <h1 className="leading-none font-sans lg:mt-0 mt-4  font-semibold lg:text-6xl flex justify-center sm:justify-normal text-4xl">
+        <h1 className="leading-none font-sans lg:mt-0 mt-4  font-semibold lg:text-4xl flex justify-center sm:justify-normal text-4xl">
           Rishab Yadav
         </h1>
 
-        <p className=" mt-3 lg:text-start text-center  text-lg">
+        <p className=" mt-3 lg:text-start text-foreground/60 text-center font-extralight  text-lg">
           {`I love building cool stuff and experimenting with new ideas. I've worked on a variety of projects.`}
         </p>
       </div>
