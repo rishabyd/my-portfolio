3a743dd30b39c73def71f6b66ec5d5dbe6c1a7c7
rishabyd <elitebot47@gmail.com>
2026-03-26 03:21:42 +0530

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@
 
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
 })
